5253c5a5ae1d714411a5ca64cc9bc0a100e9f346
PeterYangCoder <sen0238@163.com>
2026-06-07 16:06:33 +0800

    remove generated keil fiels

--- mdk/Objects/gd32a7xx_hwsem.d
+++
@@ -1,2 +0,0 @@
-./objects/gd32a7xx_hwsem.o: \
-  ..\firmware\GD32A7xx_standard_peripheral\Source\gd32a7xx_hwsem.c
--- mdk/Objects/gd32a7xx_mcmu.d
+++
@@ -1,2 +0,0 @@
-./objects/gd32a7xx_mcmu.o: \
-  ..\firmware\GD32A7xx_standard_peripheral\Source\gd32a7xx_mcmu.c
